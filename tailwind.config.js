eded5e5ed2cc8d46ba3c4f5be5b07528d49b17b8
Riya Gupta git config --global user.email riyag0841@gmail.com <riyag0841@gmail.com>
2022-01-25 17:13:00 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        dark:{
+          500:"#404366",
+          900:"#000",
+          700:"#4B4B4B",
+          200:"#9092A9"
+        },
+        primary:{
+          500:"#D97559"
+        }
+      },
+      backgroundColor:["active"]
+    }
+  },
+  plugins: [],
+}
