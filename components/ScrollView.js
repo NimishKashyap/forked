6852cbb8920782d4a0610781e268a0835d6fd79e
NimishKashyap <nimish.kashyap2020@vitstudent.ac.in>
2022-01-27 17:44:07 +0530

    scrollview fixed

--- components/ScrollView.js
+++ components/ScrollView.js
@@ -4,8 +4,13 @@ import Vector from "./Icons/Vector";
 function ScrollView() {
   return (
     <div>
-      <ul className="flex justify-start overflow-auto scroll-smooth border-2 border-[rgba(125,125,125,0.4)] rounded-lg p-3 md:mx-16 mt-5">
+      <ul className="flex md:mx-[50px] justify-start overflow-auto scroll-smooth border-2 border-[rgba(125,125,125,0.4)] rounded-lg p-3 mt-5">
 
+        <ScrollViewItems icon={<Vector />} name={"COLLEGE INFO"} />
+        <ScrollViewItems icon={<Vector />} name={"COLLEGE INFO"} />
+        <ScrollViewItems icon={<Vector />} name={"COLLEGE INFO"} />
+        <ScrollViewItems icon={<Vector />} name={"COLLEGE INFO"} />
+        <ScrollViewItems icon={<Vector />} name={"COLLEGE INFO"} />
         <ScrollViewItems icon={<Vector />} name={"COLLEGE INFO"} />
         <ScrollViewItems icon={<Vector />} name={"COLLEGE INFO"} />
         <ScrollViewItems icon={<Vector />} name={"COLLEGE INFO"} />
