150a0bfd26027965df427b73b4df5f567f7ab33c
NimishKashyap <nimish.kashyap2020@vitstudent.ac.in>
2022-01-27 20:55:41 +0530

    responsiveness done

--- components/Admission/SeatReservation.js
+++ components/Admission/SeatReservation.js
@@ -2,23 +2,27 @@ import React from "react";
 
 function AdmissionDetails() {
   return (
-    <div className="my-5 flex justify-items-stretch md:my-[3rem] mx-5 text-[#404366]">
-      <div className="flex flex-col md:flex-row md:mx-5 w-2/3 md:w-full">
-        <ul className="flex md:flex-col justify-start items-start md:items-center md:w-1/4 my-3">
-          <span className="text-base mr-5 md:mr-auto md:text-xl">Scheduled Caste</span>
-          <span className="text-[#9092A9] text-lg">15%</span>
+    <div className="my-5 flex md:my-[3rem] mx-5 text-[#404366]">
+      <div className="flex flex-col md:flex-row items-start md:mx-5 w-2/3 md:w-full text-center">
+        <ul className="flex md:flex-col md:items-center md:justify-center md:w-1/4 my-3">
+          <p className="text-base mr-5 md:text-xl">Scheduled Caste</p>
+          <p className="text-[#9092A9] text-lg">15%</p>
         </ul>
-        <ul className="flex md:flex-col justify-start items-start md:items-center md:w-1/4 my-3">
-          <span className="text-base mr-5 md:mr-auto md:text-xl">Scheduled Tribe</span>
-          <span className="text-[#9092A9] text-lg">7.5%</span>
+        <ul className="flex md:flex-col md:items-center md:justify-center md:w-1/4 my-3">
+          <p className="text-base mr-5 md:text-xl">Scheduled Tribe</p>
+          <p className="text-[#9092A9] text-lg">7.5%</p>
         </ul>
-        <ul className="flex md:flex-col justify-start items-start md:items-center md:w-1/4 my-3">
-          <span className="text-base mr-5 md:mr-auto md:text-xl">Economically Weaker Section</span>
-          <span className="text-[#9092A9] text-lg">10%</span>
+        <ul className="flex md:flex-col md:items-center md:justify-center md:w-1/4 my-3">
+          <p className="text-base mr-5 md:text-xl">
+            Economically Weaker Sections{" "}
+          </p>
+          <p className="text-[#9092A9] text-lg">10%</p>
         </ul>
-        <ul className="flex md:flex-col justify-start items-start md:items-center md:w-1/4 my-3">
-          <span className="text-base mr-5 md:mr-auto md:text-xl break-words">Persons with Benchmark Disabilities</span>
-          <span className="text-[#9092A9] text-lg">5%</span>
+        <ul className="flex md:flex-col md:items-center md:justify-center md:w-1/4 my-3">
+          <p className="text-base mr-5 md:text-xl">
+            Economically Weaker Sections{" "}
+          </p>
+          <p className="text-[#9092A9] text-lg">10%</p>
         </ul>
       </div>
     </div>
