cb431fd651d5e8f7d3cde913b1082632e2af9486
NimishKashyap <nimish.kashyap2020@vitstudent.ac.in>
2022-01-27 21:11:49 +0530

    dropdown added

--- components/FAQDropDown.js
+++ components/FAQDropDown.js
@@ -1,30 +1,16 @@
-import React from "react";
-import CaretIcon from "./Icons/CaretIcon";
+import React, { useState } from "react";
+import FAQDropDownItem from "./FAQDropDownItem";
 
 function FAQDropDown() {
+  const [checked, setChecked] = useState(false);
   return (
     <div className="w-auto md:w-1/2 my-3 px-4">
       <ul className="flex flex-col h-full justify-evenly">
-        <li className="flex items-center justify-between border-2 p-2 px-3 rounded-md">
-          What is the general process to apply for schorlarships in India?
-          <CaretIcon />
-        </li>
-        <li className="flex items-center justify-between border-2  p-2 px-3 rounded-md">
-          What is the general process to apply for schorlarships in India?
-          <CaretIcon />
-        </li>
-        <li className="flex items-center justify-between border-2  p-2 px-3 rounded-md">
-          What is the general process to apply for schorlarships in India?
-          <CaretIcon />
-        </li>
-        <li className="flex items-center justify-between border-2  p-2 px-3 rounded-md">
-          What is the general process to apply for schorlarships in India?
-          <CaretIcon />
-        </li>
-        <li className="flex items-center justify-between border-2 p-2 px-3 rounded-md">
-          What is the general process to apply for schorlarships in India?
-          <CaretIcon />
-        </li>
+        <FAQDropDownItem />
+        <FAQDropDownItem />
+        <FAQDropDownItem />
+        <FAQDropDownItem />
+        <FAQDropDownItem />
       </ul>
     </div>
   );
