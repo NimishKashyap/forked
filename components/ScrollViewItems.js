4de287d42d9bdc83d33f55ce1f0d5095aa6f1f77
NimishKashyap <nimish.kashyap2020@vitstudent.ac.in>
2022-01-29 13:38:10 +0530

    icons added

--- components/ScrollViewItems.js
+++ components/ScrollViewItems.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function ScrollViewItems({ svg, icon, name }) {
   return (
-    <div className="flex max-w-[80%] w-16 min-w-fit mx-5 flex-col items-center">
+    <div className="flex max-w-[80%] w-16 min-w-fit mx-3 md:mx-10 flex-col items-center justify-center">
       {icon}
       <p className="text-[rgba(100,100,100,1)] text-xs md:text-sm sm:text-base">{name}</p>
     </div>
