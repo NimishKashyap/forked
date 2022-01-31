06d0d29b45f68c0c51eac6ad3fbb359ea2a58238
NimishKashyap <nimish.kashyap2020@vitstudent.ac.in>
2022-01-31 16:42:48 +0530

    placement page done

--- pages/placements.js
+++ pages/placements.js
@@ -4,6 +4,9 @@ import Navbar from "../components/Navbar";
 import Banner from "../components/Admission/Banner";
 import ScrollView from "../components/ScrollView";
 import PlacementComp from "../components/Placements/PlacementComp";
+import TopCollege from "../components/TopCollege";
+import Footer from "../components/Footer";
+import Review from "../components/Review";
 function placements() {
   return (
     <>
@@ -20,6 +23,24 @@ function placements() {
 
         <PlacementComp />
       </main>
+      <section className="md:mx-[4rem]">
+        <h1 className="text-dark-500 text-xl md:text-3xl mt-5 md:mt-[3rem]">
+          Reviews on IIMA
+        </h1>
+        <Review />
+        <Review />
+        <Review />
+        <button className="border-primary-500 border-2 mx-5 my-5 px-5 py-2 rounded-md text-dark-500">
+          SEE MORE REVIEWS
+        </button>
+      </section>
+      <section className="mx-[2rem] md:mx-[4rem]">
+        <h1 className="text-dark-500 text-3xl my-5 mx-5 md:my-[3rem]">
+          Top MBA Colleges
+        </h1>
+        <TopCollege />
+      </section>
+      <Footer />
     </>
   );
 }
